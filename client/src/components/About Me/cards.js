39de8febda9d0c38b61d24cd120c32996d69c64e
YevFED <yevheniidev2@gmail.com>
2025-05-14 21:02:04 +0300

    update

--- client/src/components/About Me/cards.js
+++ client/src/components/About Me/cards.js
@@ -1,6 +1,6 @@
 const cards = [
   {
-    question: "Who am i?",
+    question: "Who am i ?",
     start: true,
     answer:
       " My name is Yevhenii and i'm a student from Ukraine , i'm 20 years old I wonder to study Development",
